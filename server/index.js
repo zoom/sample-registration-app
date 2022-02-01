9e62be2252b753777b3320ba8e097f0e5f964482
Max Mansfield <max.m.mansfield@gmail.com>
2022-01-31 17:03:50 -0700

    formatted inex.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import http from 'http'
 import debug from 'debug'
 import mongoose from 'mongoose'
-import { MongoMemoryServer } from 'mongodb-memory-server'
+import {MongoMemoryServer} from 'mongodb-memory-server'
 import app from '../app.js'
 
 // connect to the in-memory mongodb instance
@@ -18,29 +18,27 @@ function normalizePort(port) {
 
     // named pipe
     if (Number.isNaN(p))
-        return port;
+        return port
 
     // port number
     if (p >= 0)
-        return p;
+        return p
 
-    return false;
+    return false
 }
 
 // Get port from environment and store in Express.
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000')
 app.set('port', port);
 
 // Create HTTP server
 const server = http.createServer(app);
 
 // Listen on provided port, on all network interfaces
-server.listen(port);
+server.listen(port)
 
 // disconnect the DB when the server closes
-server.on('close', async () => {
-    await mongoose.disconnect()
-})
+server.on('close', async () => mongoose.disconnect())
 
 // let the user know we're serving
 server.on('listening', () => {
@@ -48,16 +46,16 @@ server.on('listening', () => {
     const bind = typeof addr === 'string'
         ? `pipe ${addr}`
         : `http://localhost:${addr.port}`;
-    debug('SRA:server')(`Listening on ${bind}`);
+    debug('SRA:server')(`Listening on ${bind}`)
 });
 
 // disconnect from the DB on error
 server.on('error', async (error) => {
     await mongoose.disconnect()
 
-    if (error.syscall !== 'listen') {
+    if (error.syscall !== 'listen')
         throw error;
-    }
+
 
     const bind = typeof port === 'string'
         ? `Pipe ${port}`
@@ -67,13 +65,13 @@ server.on('error', async (error) => {
     switch (error.code) {
         case 'EACCES':
             /* eslint-disable-next-line no-console */
-            console.error(`${bind} requires elevated privileges`);
-            process.exit(1);
+            console.error(`${bind} requires elevated privileges`)
+            process.exit(1)
             break;
         case 'EADDRINUSE':
             /* eslint-disable-next-line no-console */
-            console.error(`${bind} is already in use`);
-            process.exit(1);
+            console.error(`${bind} is already in use`)
+            process.exit(1)
             break;
         default:
             throw error;
